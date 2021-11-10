d0189ffd6cb05bcd0d4f99d4b1a67ec15b268903
Alex Kalmuk <alexkalmuk@gmail.com>
2021-11-10 17:16:50 +0300

    add simple test

---
+++ test/cycle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+int cycle(void)
+{
+	int i, j;
+	int dst = 0;
+
+	for (i = 0; i < 100000000; i++) {
+		j = i * 2;
+	}
+
+	asm ("mov %%ecx, %0\n\t"
+		: "=r" (dst)
+		:);
+
+	printf("dst = 0x%08x\n", dst);
+
+	return j;
+}
+ 
+int main(void)
+{
+	asm ("mov $0, %%ecx\n\t"
+		: :);
+
+	printf("Res = %d\n", cycle());
+
+    return 0;
+}
