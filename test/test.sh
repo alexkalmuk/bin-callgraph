2dfa98b9295a51c411b4f371e7ffa7bc70549e32
Alex Kalmuk <alexkalmuk@gmail.com>
2021-11-22 17:19:05 +0300

    Build tests

--- test/test.sh
+++ test/test.sh
@@ -3,14 +3,12 @@
 set -e
 
 TEST=$1
-TEST_DIR=../test
+INSTR_FILE=$(find ../test -type f -name $TEST.txt)
 
 echo "Running test $TEST"
 
-rm -f $TEST
 rm -f $TEST.out
-gcc -O0 -g3 $TEST_DIR/$TEST.c -o $TEST
-./instr -f $TEST_DIR/$TEST.txt $TEST && chmod +x $TEST.out
+./instr -f $INSTR_FILE $TEST && chmod +x $TEST.out
 
 echo "Run out ELF"
 ./$TEST.out
